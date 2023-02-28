4472f7768884b38ea9f7c15114964e66355040fd
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2023-02-28 12:59:49 -0600

    Se escucho difusiones de boot del sistema y mode airplane

--- MisBroadcastReceiverTraininng/app/src/main/java/net/ivanvega/misbroadcastreceivertraininng/MainActivity.kt
+++ MisBroadcastReceiverTraininng/app/src/main/java/net/ivanvega/misbroadcastreceivertraininng/MainActivity.kt
@@ -1,11 +1,36 @@
 package net.ivanvega.misbroadcastreceivertraininng
 
+import android.content.BroadcastReceiver
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    val br: BroadcastReceiver = MyBroadcastReceiver()
+    val filter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION).apply {
+        addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        registerReceiver(br, filter)
     }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(br)
+    }
+
 }
--- MisBroadcastReceiverTraininng/app/src/main/java/net/ivanvega/misbroadcastreceivertraininng/MyBroadcastReceiver.kt
+++ MisBroadcastReceiverTraininng/app/src/main/java/net/ivanvega/misbroadcastreceivertraininng/MyBroadcastReceiver.kt
@@ -3,6 +3,7 @@ package net.ivanvega.misbroadcastreceivertraininng
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.AsyncTask
 import android.util.Log
 import android.widget.Toast
 
@@ -10,13 +11,37 @@ private const val TAG = "MyBroadcastReceiver"
 
 class MyBroadcastReceiver : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
-            StringBuilder().apply {
+            /*StringBuilder().apply {
                 append("Action: ${intent.action}\n")
                 append("URI: ${intent.toUri(Intent.URI_INTENT_SCHEME)}\n")
                 toString().also { log ->
                     Log.d(TAG, log)
                     Toast.makeText(context, log, Toast.LENGTH_LONG).show()
                 }
+            }*/
+            val pendingResult: PendingResult = goAsync()
+            val asyncTask = Task(pendingResult, intent)
+            asyncTask.execute()
+        }
+
+    private class Task(
+        private val pendingResult: PendingResult,
+        private val intent: Intent
+    ) : AsyncTask<String, Int, String>() {
+
+        override fun doInBackground(vararg params: String?): String {
+            val sb = StringBuilder()
+            sb.append("Action: ${intent.action}\n")
+            sb.append("URI: ${intent.toUri(Intent.URI_INTENT_SCHEME)}\n")
+            return toString().also { log ->
+                Log.d(TAG, log)
             }
         }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            // Must call finish() so the BroadcastReceiver can be recycled.
+            pendingResult.finish()
+        }
     }
+}
